df6f260dd6e2939c712af8fa6520351974eeb3cb
NikRedFox <nikolasbs11@gmail.com>
2026-03-20 16:54:00 -0300

    Feat: Teste de Cypress para QA de uma pagina web

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+describe('template spec', () => {
+  it('Logar com sucesso', () => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('4dt@gmail.com')
+    cy.get('#password').type('4DT')
+    cy.contains('button', 'Entrar').click();
+  })
+
+  it('Campo em branco', () => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:3000')
+
+    cy.contains('button', 'Entrar').click();
+
+    cy.contains('Ei, não esqueça de digitar seu email!')
+    cy.contains('Você precisa de uma senha para entrar! 🔒')    
+  }) 
+  
+  it('Email e Senha invalidos', () => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('3dt@gmail.com')
+    cy.get('#password').type('2DT')
+    
+    cy.contains('button', 'Entrar').click();
+
+    cy.get('.title').contains('Acesso negado! Tente novamente.') 
+  }) 
+})
